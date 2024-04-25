4b8934e15fef3d01069ed9dc574e428aae665e41
= <abhishekdk0304@gmail.com>
2024-04-25 10:44:49 +0530

    25-04-2024 form functionality finished.

--- js/email.js
+++ js/email.js
@@ -19,10 +19,41 @@
       .then(function(response) {
         // Handle successful email sending
         alert("Email sent successfully!");
-        return;
+        location.reload();
       }, function(error) {
         // Handle error
         console.error('FAILED...', error);
       });
-
+      
   }
+
+
+   // Function to send email using EmailJS
+ function sendMailContactform(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Check if form is valid
+  if (!event.target.checkValidity()) {
+    alert("Please fill out all fields correctly.");
+    return; // Exit the function if form is invalid
+  }
+  let parms = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    question: document.getElementById("question").value,
+    message: document.getElementById("message").value,
+  };
+
+
+  // Send email using EmailJS
+  emailjs.send("service_ka5p9er", "template_u7xsfnk", parms)
+    .then(function(response) {
+      // Handle successful email sending
+      alert("Email sent successfully!");
+      location.reload();
+    }, function(error) {
+      // Handle error
+      console.error('FAILED...', error);
+    });
+    
+}
